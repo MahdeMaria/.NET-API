39199969f44d619bb6b3bf330519d2c61564e0d0
Maria <mariae.f.gurgel@gmail.com>
2025-01-15 15:31:02 -0300

    Adicionando Endpoints

--- CadastroUsuario/Program.cs
+++ CadastroUsuario/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo
     {
-       Title = "Cadastro de Usuário API",
+        Title = "Cadastro de Usuário API",
         Version = "v1",
         Description = "API para gerenciar cadastro de usuários.",
     });
@@ -27,8 +27,55 @@ using (var scope = app.Services.CreateScope())
     dbContext.Database.EnsureCreated();
 }
 
+app.MapGet("/usuarios", async (UsuarioDb db) =>
+    await db.Usuarios.ToListAsync())
+    .WithTags("Listar usuarios");
+
+app.MapGet("/usuarios/{id}", async (int id, UsuarioDb db) =>
+    await db.Usuarios.FindAsync(id)
+        is Usuario usuario
+            ?   Results.Ok(usuario)
+            :   Results.NotFound())
+            .WithTags("Listar usuario por Id");
+
+app.MapPost("usuarios", async (Usuario usuario, UsuarioDb db) =>
+{
+    db.Usuarios.Add(usuario);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/usuarios/{usuario.Id}", usuario);
+})
+    .WithTags("Adicionar Usuario");
+
+app.MapPut("usuarios/{id}", async (int id, Usuario inputUsuario, UsuarioDb db) =>
+{
+    var usuario = await db.Usuarios.FindAsync(id);
+
+    if(usuario is null) return Results.NotFound();
+
+    usuario.Nome = inputUsuario.Nome;
+    usuario.Idade = inputUsuario.Idade;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+})
+    .WithTags("Update usuario por Id");
+
+app.MapDelete("/usuarios/{id}", async (int id, UsuarioDb db) =>
+{
+    if (await db.Usuarios.FindAsync(id) is Usuario usuario)
+    {
+        db.Usuarios.Remove(usuario);
+        await db.SaveChangesAsync();
+        return Results.Ok(usuario);
+    }
+        return Results.NotFound();
+
+})
+    .WithTags("Deletar usuario por Id");
+
 app.UseSwagger();
 app.UseSwaggerUI();
-
 
 app.Run();
--- CadastroUsuario/Usuario.cs
+++ CadastroUsuario/Usuario.cs
@@ -1,6 +1,6 @@
 public class Usuario
 {
     public int Id {get; set;}
-    public string Name {get; set;} = string.Empty;
+    public string Nome {get; set;} = string.Empty;
     public int Idade {get; set;}
 }
